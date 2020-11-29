daa5a328b739ce2fe7c0af5e4efc7a2bbfb8835c
hadassadanesh <hadassahadassa321@gmail.com>
2020-11-29 07:30:51 -0500

    AnomalyDetector.h file

--- AnomalyDetector.h
+++ AnomalyDetector.h
@@ -1,26 +1,27 @@
+#ifndef ANOMALYDETECTOR_H_
+#define ANOMALYDETECTOR_H_
+
+#include "math.h"
+#include "timeseries.h"
 #include <string>
 #include <vector>
-using std::string;
-using std:: vector;
+using namespace std;
 
-class TimeSeries{
+class AnomalyReport
+{
 public:
-    void returnListOfFeatures();
-    void supplyListOfFeatures();
-    void addALine();
-    void findValOfFeature();
-    void loadFromCsv();
+  const string description;
+  const long timeStep;
+  AnomalyReport(string description, long timeStep)
+      : description(description), timeStep(timeStep) {}
 };
-class AnomalyReport{
+
+class TimeSeriesAnomalyDetector
+{
 public:
-    const string description;
-    const long timeStep;
-    AnomalyReport(string description, long timeStep) :
-    description(description),timeStep(timeStep){}
+  virtual void learnNormal(const TimeSeries &ts) = 0;
+  virtual vector<AnomalyReport> detect(const TimeSeries &ts) = 0;
+  virtual ~TimeSeriesAnomalyDetector() {}
 };
-class TimeSeriesAnomalyDetector {
-public:
-    virtual void learnNormal(const TimeSeries& ts)=0;
-    virtual vector<AnomalyReport> detect(const TimeSeries& ts)=0;
-    virtual ~TimeSeriesAnomalyDetector(){}
-};
+
+#endif /* ANOMALYDETECTOR_H_ */
