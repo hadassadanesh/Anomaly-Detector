a16a515ee5e43d67c34f05100b31c9c255a17d8d
hadassadanesh <hadassahadassa321@gmail.com>
2020-11-29 07:33:04 -0500

    SimpleAnomalyDetector.h file

--- SimpleAnomalyDetector.h
+++ SimpleAnomalyDetector.h
@@ -1,22 +1,49 @@
-#include <string>
-#include <vector>
-using std::vector;
-using std:: string;
+#ifndef SIMPLEANOMALYDETECTOR_H_
+#define SIMPLEANOMALYDETECTOR_H_
+
+/*
+ * SimpleAnomalyDetector.h
+ *
+ * Author: 322567041 , Hadassa Danesh
+ */
+
 #include "anomaly_detection_util.h"
 #include "AnomalyDetector.h"
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
+using namespace std;
 
-struct correlatedFeatures{
-    string feature1,feature2;
+struct correlatedFeatures
+{
+    string feature1, feature2; // names of the correlated features
     float corrlation;
     Line lin_reg;
     float threshold;
 };
 
-class SimpleAnomalyDetector:public TimeSeriesAnomalyDetector{
+class SimpleAnomalyDetector : public TimeSeriesAnomalyDetector
+{
+    vector<correlatedFeatures> cf;
+
 public:
-      SimpleAnomalyDetector();
-      virtual ~SimpleAnomalyDetector();
-      virtual void learnNormal(const TimeSeries& ts);
-      virtual vector<AnomalyReport> detect(const TimeSeries& ts);
-      vector<correlatedFeatures> getNormalModel();
+    SimpleAnomalyDetector();
+    virtual ~SimpleAnomalyDetector();
+    string *findMostCorrelatedFeature(string feature, const TimeSeries &ts);
+    void getLinearReg(correlatedFeatures &correlatedStruct, const TimeSeries &ts);
+
+    virtual void learnNormal(const TimeSeries &ts);
+    virtual vector<AnomalyReport> detect(const TimeSeries &ts);
+
+    vector<correlatedFeatures> getNormalModel()
+    {
+        return cf;
+    }
+    vector<correlatedFeatures> *getCf()
+    {
+        return &cf;
+    }
 };
+
+#endif /* SIMPLEANOMALYDETECTOR_H_ */
